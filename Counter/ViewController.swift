f6355e1874af37fce3cf5545b633486577e01cb5
Anton <anton-rachkov@mail.ru>
2026-04-25 12:39:21 +0300

    Добавлен счетчик

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -2,18 +2,24 @@
 //  ViewController.swift
 //  Counter
 //
-//  Created by никита  on 21.04.2026.
+//  Created by Антон Рачков on 21.04.2026.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var score: UILabel!
+    var count = 0
     override func viewDidLoad() {
         super.viewDidLoad()
+        score.text = "Значение счётчика: 0"
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func button(_ sender: UIButton) {
+        count += 1
+        score.text = "Значение счётчика: \(count)"
+    }
 }
 
